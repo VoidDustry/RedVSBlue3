7c5510e34e2c31187df9c6b2e23bc75bc0f40a70
tomoko <taras130466@gmail.com>
2023-06-04 18:25:34 +0200

    (forgot about static)

--- src/main/java/net/voiddustry/redvsblue3/utils/Random.java
+++ src/main/java/net/voiddustry/redvsblue3/utils/Random.java
@@ -9,20 +9,20 @@ import java.util.stream.StreamSupport;
 @SuppressWarnings("unused")
 public class Random {
 
-    public int getRandomInt(int min, int max) {
+    public static int getRandomInt(int min, int max) {
         return (int) Math.floor(Math.random() *(max - min + 1) + min);
     }
 
-    public Tile getRandomTile(boolean airOnly) {
+    public static Tile getRandomTile(boolean airOnly) {
         // if airOnly == true, method would return empty tile.
 
         if (!airOnly) {
-            return Vars.world.tiles.geti(this.getRandomInt(0, Vars.world.width() * Vars.world.height()));
+            return Vars.world.tiles.geti(getRandomInt(0, Vars.world.width() * Vars.world.height()));
         }
 
         List<Tile> tileList = StreamSupport.stream(Vars.world.tiles.spliterator(), false)
                 .filter(tile -> tile.block().isAir())
                 .toList();
-        return tileList.get(this.getRandomInt(0, tileList.size()));
+        return tileList.get(getRandomInt(0, tileList.size()));
     }
 }
