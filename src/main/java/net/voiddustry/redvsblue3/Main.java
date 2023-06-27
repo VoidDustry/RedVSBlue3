2a3d435c835853dedd5d9dca9dc79583a7f5e9dd
tomoko <taras130466@gmail.com>
2023-06-27 21:12:27 +0200

    add commands initialization

--- src/main/java/net/voiddustry/redvsblue3/Main.java
+++ src/main/java/net/voiddustry/redvsblue3/Main.java
@@ -1,10 +1,22 @@
 package net.voiddustry.redvsblue3;
 
+import arc.util.CommandHandler;
 import mindustry.mod.Plugin;
+import net.voiddustry.redvsblue3.commands.Commands;
 
 public class Main extends Plugin {
     @Override
     public void init() {
         Loader.load();
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        Commands.registerClientCommands(handler);
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+        Commands.registerServerCommands(handler);
     }
 }
