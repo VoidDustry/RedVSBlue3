51a5b52b6f2ead140f4c1335f74ff6949c6897e7
tomoko <taras130466@gmail.com>
2023-06-27 15:00:08 +0200

    new methods

--- src/main/java/net/voiddustry/redvsblue3/game/domain/CrystalData.java
+++ src/main/java/net/voiddustry/redvsblue3/game/domain/CrystalData.java
@@ -10,27 +10,44 @@ public class CrystalData {
     private Tile tileOn;
     private Integer infectPercentage;
 
-    public CrystalData(String type, int i, Team team, Tile tile) {
+    public CrystalData(String type, int i, Team team, Tile tile, int infectPercentage) {
         this.type = type;
         this.number = i;
         this.teamOwner = team;
         this.tileOn = tile;
+        this.infectPercentage = infectPercentage;
     }
 
     public void setType(String type) {
         this.type = type;
     }
 
+    public String getType() {
+        return this.type;
+    }
+
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
     }
 
     public void setTeamOwner(Team team) {
         this.teamOwner = team;
     }
 
+    public Team getTeamOwner() {
+        return this.teamOwner;
+    }
+
     public void setTileOn(Tile tile) {
         this.tileOn = tile;
+    }
+
+    public Tile getTileOn() {
+        return this.tileOn;
     }
 
     public void setInfectPercentage(int i) {
@@ -43,5 +60,9 @@ public class CrystalData {
 
     public void substractInfectPercentage(int i) {
         this.infectPercentage -= i;
+    }
+
+    public int getInfectPercentage() {
+        return this.infectPercentage;
     }
 }
